fec9a3e3482e3b329ddb00524c0cdb428a1a9b2b
Ansh-info <anshkumar.info@gmail.com>
2025-05-01 21:41:28 +0200

    Add: stg for fx sp500
    Co-authored-by: Apoorva Gupta <apoorvaagupta.info@gmail.com>

---
+++ models/staging/stg_test_sp500.sql
@@ -0,0 +1,13 @@
+{{ config(
+    materialized='view'
+) }}
+
+-- Staging model: synthetic S&P 500 index OHLCV data
+select
+  date,
+  open,
+  high,
+  low,
+  close,
+  volume
+from `{{ target.project }}.{{ target.dataset }}.test_sp500`
